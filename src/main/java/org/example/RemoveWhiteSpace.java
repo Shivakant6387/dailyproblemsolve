39f210852ae6bb11dc9d89c77e3195f00369877f
Shivakant6387 <shivakant5875@gmail.com>
2023-01-30 11:06:24 +0530

    validate ip address

---
+++ src/main/java/org/example/RemoveWhiteSpace.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class RemoveWhiteSpace {
+    public static void main(String[] args) {
+        String str="This is an elephant";
+        str.trim();
+        System.out.println(str);
+        str=str.replace(" ","");
+        System.out.println(str);
+
+        char[]arr=str.toCharArray();
+        String tempStr="";
+        for(char c:arr){
+            if (c !=' '){
+                tempStr+=c;
+            }
+        }
+        str=tempStr;
+        System.out.println(str);
+    }
+}
