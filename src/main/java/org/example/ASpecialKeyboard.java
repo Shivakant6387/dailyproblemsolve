e18f987a43df76dcb0483572dc41b28d6e66bd14
Shivakant6387 <shivakant5875@gmail.com>
2023-02-10 17:40:34 +0530

    a special keyboard

---
+++ src/main/java/org/example/ASpecialKeyboard.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class ASpecialKeyboard {
+    public static void main(String[] args) {
+        int n;
+        for (n=1;n<=20;n++)
+            System.out.println("Maximum of A`s with keyStrokes is "+n+findoptimal(n)  );
+    }
+    static int findoptimal(int n){
+        if (n<=6)
+            return n;
+        int max=0;
+        int b;
+        for (b=n-3;b>=1;b--){
+            int curr=(n-b-1)*findoptimal(b);
+            if (curr>max)
+                max=curr;
+        }
+        return max;
+    }
+}
