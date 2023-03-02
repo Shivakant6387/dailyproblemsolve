643885fa3973edf8ab55d138de5fc1ea0208f645
Shivakant6387 <shivakant5875@gmail.com>
2023-03-02 12:43:51 +0530

    added problem solving;

---
+++ src/main/java/org/example/Moveallnegativenumber.java
@@ -0,0 +1,13 @@
+package org.example;
+import java.util.Arrays;
+public class Moveallnegativenumber {
+    public static void main(String[] args) {
+        int[] arr = { -1, 2, -3, 4, 5, 6, -7, 8, 9 };
+        move(arr);
+        for (int e : arr)
+            System.out.print(e + " ");
+    }
+    public static void move(int[] arr) {
+        Arrays.sort(arr);
+    }
+}
