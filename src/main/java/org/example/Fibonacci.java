4abe387973ba36bfe2d598225de680b7566ec8da
Shivakant6387 <shivakant5875@gmail.com>
2023-05-10 13:45:41 +0530

    added all problem

---
+++ src/main/java/org/example/Fibonacci.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        printFibonacci(10);
+        int n=10;
+        for (int i=0;i<n;i++){
+            System.out.print(fibonacci(i)+" ");
+        }
+    }
+    public static void printFibonacci(int n){
+        int a=0;
+        int b=1;
+        int c=1;
+        for (int i=1;i<=n;i++){
+            System.out.print(a+" ");
+            a=b;
+            b=c;
+            c=a+b;
+        }
+
+    }
+    public static int fibonacci(int count){
+        if(count<=1)
+            return count;
+        return fibonacci(count-1)+fibonacci(count-2);
+    }
+}
---
+++ src/main/java/org/example/PalindromeString.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+        System.out.println(checkPalindromeString("shiva"));
+        System.out.println(isPalindrome("Shiva"));
+
+    }
+    public static boolean checkPalindromeString(String input){
+        boolean result=true;
+        int length=input.length();
+        for (int i=0;i<length/2;i++){
+            if (input.charAt(i)!=input.charAt(length-i-1)){
+                result=false;
+                break;
+            }
+        }
+        return result;
+    }
+    public static boolean isPalindrome(String input){
+        int i=0,j=input.length()-1;
+        while (i<j){
+            if (input.charAt(i)!=input.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
