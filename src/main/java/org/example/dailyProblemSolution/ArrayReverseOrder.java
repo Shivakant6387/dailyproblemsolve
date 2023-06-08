8a917a2d0b30571d70ed4c701a72abb71744a365
Shivakant6387 <shivakant5875@gmail.com>
2023-06-08 10:05:48 +0530

    array reverse order

---
+++ src/main/java/org/example/dailyProblemSolution/ArrayReverseOrder.java
@@ -0,0 +1,14 @@
+package org.example.dailyProblemSolution;
+
+public class ArrayReverseOrder {
+    public static void reverseOrder(int nums[],int length){
+        for (int i=length-1;i>=0;i--){
+            System.out.print(nums[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int nums[]={10,45,30,50,20};
+        int length=nums.length;
+        reverseOrder(nums,length);
+    }
+}
